531da04efb15a5e2457f6bb22be18c1753511cf6
tyjtyj <tyjtyj@users.noreply.github.com>
2020-07-18 04:07:42 +0800

    Update sensor.py
    Fix SSL error

--- custom-component/sgpm25/sensor.py
+++ custom-component/sgpm25/sensor.py
@@ -1,15 +1,14 @@
 import logging
 import json
 from datetime import timedelta, datetime
-
-
+from requests import Session
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 import voluptuous as vol
-													   
-
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.components.rest.sensor import RestData
+#from homeassistant.components.rest.sensor import RestData
 
+#from homeassistant.custom_componets.sgnea.sensor import NEARestData
 from homeassistant.const import (
     CONF_NAME, CONF_RESOURCE, STATE_UNKNOWN)
 																	  
@@ -28,7 +27,7 @@ DEFAULT_RESOURCE = 'https://api.data.gov.sg/v1/environment/pm25?date_time='
 DEFAULT_NAME = 'SG 1Hour PM2.5'
 SCAN_INTERVAL = timedelta(minutes=1800)
 PARALLEL_UPDATES = 1
-
+DEFAULT_TIMEOUT = 10
 TIMEOUT = 10
 
 
@@ -52,25 +51,25 @@ def setup_platform(hass, config, add_entities,
     method = 'GET'
     payload = None
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
-    verify_ssl = 0
+    verify_ssl = True
 					 
     area = config.get(CONF_AREA)
 
     resourcenow = resource + datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     auth = None
-    rest = RestData(method, resourcenow, auth, headers, payload, verify_ssl)
+    rest = NEARestData(method, resourcenow, auth, headers, payload, verify_ssl)
     
     rest.update()
 
     if rest.data is None:
         raise PlatformNotReady
 
-    add_entities([NeaSensorPM25(name, resource, headers, area)], True)       
+    add_entities([NeaSensorPM25(name, resource, headers, area, verify_ssl)], True)       
 
 class NeaSensorPM25(Entity):
     """Representation of a web scrape sensor."""
 
-    def __init__(self, name, resource, headers, area):
+    def __init__(self, name, resource, headers, area, verify_ssl):
         """Initialize a web scrape sensor."""
         self._name = name
         self._area = area
@@ -78,7 +77,7 @@ class NeaSensorPM25(Entity):
         self._headers = headers
         self._state = STATE_UNKNOWN
         self._unit_of_measurement = 'PM2.5'
-
+        self._verify_ssl = verify_ssl
 
     @property
     def name(self):
@@ -98,7 +97,7 @@ class NeaSensorPM25(Entity):
     def update(self):
         """Get the latest data from the source and updates the state."""
         resourcenow = self._resource + datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        rest = RestData('GET', resourcenow, None, self._headers, None, 0)
+        rest = NEARestData('GET', resourcenow, None, self._headers, None, self._verify_ssl)
         rest.update()
         try: 
             json_dict = json.loads(rest.data)
@@ -115,3 +114,49 @@ class NeaSensorPM25(Entity):
         _LOGGER.debug("The data value is: %s", rest.data)
 
         self._state = value
+
+class NEARestData:
+    """Class for handling the data retrieval."""
+
+    def __init__(
+        self, method, resource, auth, headers, data, verify_ssl, timeout=DEFAULT_TIMEOUT
+    ):
+        """Initialize the data object."""
+        self._method = method
+        self._resource = resource
+        self._auth = auth
+        self._headers = headers
+        self._request_data = data
+        self._verify_ssl = verify_ssl
+        self._timeout = timeout
+        self._http_session = Session()
+        self.data = None
+        self.headers = None
+
+    def __del__(self):
+        """Destroy the http session on destroy."""
+        self._http_session.close()
+
+    def set_url(self, url):
+        """Set url."""
+        self._resource = url
+
+    def update(self):
+        """Get the latest data from REST service with provided method."""
+        _LOGGER.debug("Updating from %s", self._resource)
+        try:
+            response = self._http_session.request(
+                self._method,
+                self._resource,
+                headers=self._headers,
+                auth=self._auth,
+                data=self._request_data,
+                timeout=self._timeout,
+                verify=self._verify_ssl,
+            )
+            self.data = response.text
+            self.headers = response.headers
+        except requests.exceptions.RequestException as ex:
+            _LOGGER.error("Error fetching data: %s failed with %s", self._resource, ex)
+            self.data = None
+            self.headers = None
